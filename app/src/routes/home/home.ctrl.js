dd71a96efe8110ba14f706e0d10b871b84dcfa35
0131ryu <131ryuji@gmail.com>
2022-03-21 17:14:58 +0900

    User 모델 구현

--- app/src/routes/home/home.ctrl.js
+++ app/src/routes/home/home.ctrl.js
@@ -1,4 +1,5 @@
 "use strict";
+const User = require("../../models/User");
 const UserStorage = require("../../models/UserStorage");
 
 const users = {
@@ -17,26 +18,30 @@ const output = {
 
 const process = {
   login: (req, res) => {
-    const id = req.body.id,
-      psword = req.body.psword;
-    //console.log(id, psword);
-
-    // const userStorage = new UserStorage();
-    console.log(UserStorage.getUsers("id", "psword"));
-
-    const response = {};
-
-    if (users.id.includes(id)) {
-      const idx = users.id.indexOf(id);
-      if (users.psword[idx] === psword) {
-        response.success = true;
-        return res.json(response);
-      }
-    }
-
-    response.success = false;
-    response.msg = "로그인에 실패하였습니다.";
+    const user = new User(req.body);
+    const response = user.login();
+    console.log(response);
     return res.json(response);
+    // const id = req.body.id,
+    //   psword = req.body.psword;
+    // //console.log(id, psword);
+
+    // // const userStorage = new UserStorage();
+    // console.log(UserStorage.getUsers("id", "psword"));
+
+    // const response = {};
+
+    // if (users.id.includes(id)) {
+    //   const idx = users.id.indexOf(id);
+    //   if (users.psword[idx] === psword) {
+    //     response.success = true;
+    //     return res.json(response);
+    //   }
+    // }
+
+    // response.success = false;
+    // response.msg = "로그인에 실패하였습니다.";
+    // return res.json(response);
   },
 };
 
